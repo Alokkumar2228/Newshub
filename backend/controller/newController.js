fd8f2de9b3ca9477e116d1a1ecfaba9ac6f8177d
Alokkumar2228 <141926450+Alokkumar2228@users.noreply.github.com>
2025-04-16 05:00:35 +0530

    env

--- backend/controller/newController.js
+++ backend/controller/newController.js
@@ -7,7 +7,7 @@ const categoryNews = async (req, res) => {
     const category = req.params.category;
     const page = req.query.page || 1;
     const pageSize = req.query.pageSize || 8;
-    
+    console.log("ENV KEY:", process.env.NEWS_API_KEY);
     try {
       const response = await axios.get('https://newsapi.org/v2/top-headlines', {
         params: {
